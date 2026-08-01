e979f96a4ab1219c7c9a498cafa6246e53f15a1f
belal-said <belalelkholy33@gmail.com>
2026-08-01 20:00:21 +0300

    little commits

--- asynchronous/promise.js
+++ asynchronous/promise.js
@@ -8,8 +8,29 @@
 //     console.log(i)
 // }
 
-new Promise((resolve, reject) => {
-    setTimeout(() => resolve("value"), 2000);
-})
-    .finally(() => console.log("Promise ready"))
-    .then((result) => console.log(result));
+// new Promise((resolve, reject) => {
+//     setTimeout(() => resolve("value"), 2000);
+// })
+//     .finally(() => console.log("Promise ready"))
+//     .then((result) => console.log(result));
+
+// Promise.all( [
+//   new Promise(resolve => setTimeout(() => resolve(1), 3000)), // 1
+//   new Promise(resolve => setTimeout(() => resolve(2), 2000)), // 2
+//   new Promise(resolve => setTimeout(() => resolve(3), 1000))  // 3
+// ]).then((value) => console.log(value));
+
+let urls = [
+  'https://api.github.com/users/iliakan',
+  'https://api.github.com/users/remy',
+  'https://api.github.com/users/jeresig'
+];
+
+// map every url to the promise of the fetch
+let requests = urls.map(url => fetch(url));
+
+// Promise.all waits until all jobs are resolved
+Promise.all(requests)
+  .then(responses => responses.forEach(
+    response => console.log(`${response.url}: ${response.status}`)
+  ));
